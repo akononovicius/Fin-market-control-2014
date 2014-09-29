e3a7d68f5968cc6282bef8ee97f9a243a62a8239
akononovicius <aleksejus.kononovicius@gmail.com>
2014-09-29 10:29:19 +0300

    Scripts used to produce figures

---
+++ fig5.bat
@@ -0,0 +1,4 @@
+@java -Xmx1024m -jar doubleModel.jar -j 1 33 -rp 100000 -pdflim 1e-3 1e3 --limits 1e-3 0.999 -op 300 -k 0.03 -ot 0. -pars 0.1 3 3 300 -m 0 --chartistImpact
+@java -Xmx1024m -jar doubleModel.jar -j 1 33 -rp 100000 -pdflim 1e-3 1e3 --limits 1e-3 0.999 -op 300 -k 0.03 -ot 1. -pars 0.1 3 3 300 -m 2 --chartistImpact
+@java -Xmx1024m -jar doubleModel.jar -j 1 33 -rp 100000 -pdflim 1e-3 1e3 --limits 1e-3 0.999 -op 300 -k 0.03 -ot 2. -pars 0.1 3 3 300 -m 4 --chartistImpact
+@java -Xmx1024m -jar doubleModel.jar -j 1 33 -rp 100000 -pdflim 1e-3 1e3 --limits 1e-3 0.999 -op 300 -k 0.03 -ot 3. -pars 0.1 3 3 300 -m 8 --chartistImpact
---
+++ fig6.bat
@@ -0,0 +1,4 @@
+@java -Xmx1024m -jar doubleModel.jar -j 1 33 -rp 100000 -pdflim 1e-3 1e3 --limits 1e-3 0.999 -op 300 -k 0.03 -ot 0. -pars 0.1 3 3 300 -m 0 --symmetricImpact
+@java -Xmx1024m -jar doubleModel.jar -j 1 99 -rp 100000 -pdflim 1e-3 1e3 --limits 1e-3 0.999 -op 300 -k 0.03 -ot 1. -pars 0.1 3 3 300 -m 2 --symmetricImpact
+@java -Xmx1024m -jar doubleModel.jar -j 1 333 -rp 100000 -pdflim 1e-3 1e3 --limits 1e-3 0.999 -op 300 -k 0.03 -ot 2. -pars 0.1 3 3 300 -m 4 --symmetricImpact
+@java -Xmx1024m -jar doubleModel.jar -j 1 999 -rp 100000 -pdflim 1e-3 1e3 --limits 1e-3 0.999 -op 300 -k 0.03 -ot 3. -pars 0.1 3 3 300 -m 8 --symmetricImpact
